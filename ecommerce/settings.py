faa5e0ceeac610385470723e4508b4f87d198fbf
Barak <bachillah@gmail.com>
2019-08-18 20:16:16 +0300

    added a views.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^&ypg^ffj5vhvl$%lfc%hq%i7#4m5=)u5*j3&434iq!g3)$kht'
+SECRET_KEY = 'h(_78z392os!p1-%b=d=a0u4(c%w(zky5e*9-gd_z+r_!fcp43'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'ecommerce.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
