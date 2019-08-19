49ff9354c4cc00f9dea0c79f3313d0a8aec5ead3
Barak <bachillah@gmail.com>
2019-08-20 00:22:50 +0300

    created the login form

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,2 +1,31 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import LoginForm
+from django.contrib.auth import authenticate,login
+
+
+def LoginPage(request):
+
+    form=LoginForm(request.POST or None)
+    print("not auth")
+    if form.is_valid():
+        username=form.cleaned_data["username"]
+        password=form.cleaned_data["password"]
+        user=authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            #context['form']=LoginForm()
+            print('auth')
+            return redirect("/admin")
+        else:
+            print("error")
+    context={
+            "form":form,
+            }
+    return render(request,"auth/login.html",context)
+
+def RegisterPage(request):
+    form=LoginForm(request.POST or None)
+    if form.is_valid():
+        print(form.cleaned_data)
+    return render(request,"auth/register.html",{})
