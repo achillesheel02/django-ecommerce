408769667f73f25501eaf554c85f20407b782e45
Barak <bachillah@gmail.com>
2019-08-26 17:07:19 +0300

    product model, added price

--- products/models.py
+++ products/models.py
@@ -2,5 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    title = models.CharField(mx_length=120)
+    title = models.CharField(max_length=120)
     description = models.TextField()
+    price = models.DecimalField(decimal_places=2,max_digits=10, default=0.00)
