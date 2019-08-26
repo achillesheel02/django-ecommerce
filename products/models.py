1c80dbed7362baf4812539f41ed9509b629cd6e9
Barak <bachillah@gmail.com>
2019-08-26 19:54:38 +0300

    added a model mnager for product

--- products/models.py
+++ products/models.py
@@ -13,11 +13,22 @@ def upload_image_path(instance, filename):
     return "products/{year}/{month}/{day}/{randint}{ext}".format(randint=randint, ext=ext, year=now.strftime("%Y"), month=now.strftime("%m"),\
                                                                  day=now.strftime("%d"))
 
+
+class ProductManager(models.Manager):
+    def get_by_id(self,id):
+        qs = self.get_queryset().filter(id=id)
+        if qs.count() == 1:
+            return qs
+        return None
+
+
 class Product(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
     price = models.DecimalField(decimal_places=2,max_digits=10, default=0.00)
     image = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
+
+    objects = ProductManager()
 
     def __str__(self):
         return self.title
--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView, DetailView
 from django.shortcuts import render
+from django.http import Http404
 
 
 from .models import Product
@@ -11,3 +12,11 @@ class ProductListView(ListView):
 
 class ProductDetailView(DetailView):
     queryset = Product.objects.all()
+
+    def get_object(self, *args, **kwargs):
+        request = self.request
+        pk = self.kwargs.get('pk')
+        instance = Product.objects.get_by_id(pk)
+        if instance is None:
+            raise Http404("Product does not exist.")
+        return instance
